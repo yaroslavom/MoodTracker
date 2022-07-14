1be838309c6ad184c6966bb591f381ca4f503195
Yaroslavom <yarykomm@gmail.com>
2022-07-14 18:49:36 +0300

    SvgIcons

--- src/screens/BottomTabs.navigator.tsx
+++ src/screens/BottomTabs.navigator.tsx
@@ -3,15 +3,46 @@ import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 import HomeScreen from './Home.screen ';
 import HistoryScreen from './History.screen';
 import AnalyticsScreen from './Analytics.screen';
+import { HomeIcon, HistoryIcon, AnalyticsIcon } from '../components/Icons';
+import { theme } from '../context/theme';
 
 const BottomTabs = createBottomTabNavigator();
 
 export const BottomTabsNavigator: React.FC = () => {
   return (
-    <BottomTabs.Navigator>
-      <BottomTabs.Screen name="Home" component={HomeScreen} />
-      <BottomTabs.Screen name="History" component={HistoryScreen} />
-      <BottomTabs.Screen name="Analytics" component={AnalyticsScreen} />
+    <BottomTabs.Navigator
+      screenOptions={({ route }) => ({
+        tabBarActiveTintColor: theme.colorBlue,
+        tabBarInactiveTintColor: theme.colorGrey,
+        tabBarShowLabel: false,
+        tabBarIcon: ({ color, size }) => {
+          if (route.name === 'Home') {
+            return <HomeIcon color={color} size={size} />;
+          }
+          if (route.name === 'History') {
+            return <HistoryIcon color={color} size={size} />;
+          }
+          if (route.name === 'Analytics') {
+            return <AnalyticsIcon color={color} size={size} />;
+          }
+          return null;
+        },
+      })}>
+      <BottomTabs.Screen
+        name="Home"
+        component={HomeScreen}
+        options={{ title: "Today's Mood" }}
+      />
+      <BottomTabs.Screen
+        name="History"
+        component={HistoryScreen}
+        options={{ title: 'Past Moods' }}
+      />
+      <BottomTabs.Screen
+        name="Analytics"
+        component={AnalyticsScreen}
+        options={{ title: 'Fancy Charts' }}
+      />
     </BottomTabs.Navigator>
   );
 };
